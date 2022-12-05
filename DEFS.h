2fa0d3678c68b34cc23ad0fc0da84decd1e14b74
Mito91243 <85618190+Mito91243@users.noreply.github.com>
2022-12-05 02:17:32 +0200

    Update DEFS.h

--- DEFS.h
+++ DEFS.h
@@ -6,13 +6,47 @@
 //This file contains some global constants and definitions to be used in the project.
 enum ActionType //The actions supported (you can add more if needed)
 {
-	DRAW_RECT,		//Draw Rectangle
+	SELECT,           //Select
+	
+	DRAW_RECT,		  //Draw Shapes
+	DRAW_SQUARE,
+	DRAW_TRIANGLE,
+	DRAW_HEXAGON,
+	DRAW_CIRCLE,
+
+	COLOR_FILL,      //Color Actions
+	COLOR_DRAW,
+	BLACK,           //DOuble CHeck COLORS ARE ACTIONTYPES
+	ORANGE,
+	YELLOW,
+	RED,
+	GREEN,
+	BLUE,
+
+	MOVE,            //Various Functions
+	TO_DELETE,
+	UNDO,
+	REDO,
+	CLEARALL,
+	STARTRECORDING,
+	PAUSERECORDING,
+	PLAYRECORDING,
+	SAVE,
+	LOAD,
+	ADDVOICE,
+
+	PLAY_FIGURES,     //Play Mode Actions
+	PLAY_COLORS,
+	PLAY_COLORSANDFIGURES,
+
+
 	EXIT,			//Exit
 	TO_DRAW,		//Switch interface to Draw mode
 	TO_PLAY,		//Switch interface to Play mode
 	EMPTY,			//A click on empty place in the toolbar
 	DRAWING_AREA,	//A click on the drawing area
-	STATUS			//A click on the status bar
+	STATUS,			//A click on the status bar
+
 
 	///TODO: Add the other action types of the two modes
 };
